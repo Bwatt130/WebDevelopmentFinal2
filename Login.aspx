048b8be67e9a2c1d01a9cfd2fee8ff456f07359b
Bwatt130 <lilwatty130@gmail.com>
2025-02-04 20:03:42 -0500

    login push

---
+++ Login.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pharmacy.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FinalTest1.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="card p-4 shadow-lg" style="width: 300px;">
+        <h3 class="text-center">Login</h3>
+        <asp:TextBox ID="txtUsername" runat="server" CssClass="form-control mb-2" Placeholder="Username"></asp:TextBox>
+        <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control mb-2" TextMode="Password" Placeholder="Password"></asp:TextBox>
+        <asp:Button ID="btnLogin" runat="server" Text="Login" CssClass="btn btn-primary w-100" OnClick="btnLogin_Click" />
+        <asp:Label ID="lblMessage" runat="server" CssClass="text-danger mt-2 d-block"></asp:Label>
+    </div>
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinalTest1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] != null)
+            {
+                Response.Redirect("FinalMainPage.aspx");
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            // Hardcoded credentials (replace with database validation if needed)
+            if (username == "admin" && password == "password123")
+            {
+                Session["User"] = username; 
+                Response.Redirect("FinalMainPage.aspx"); 
+            }
+            else
+            {
+                lblMessage.Text = "Invalid username or password.";
+            }
+        }
+    }
+}
